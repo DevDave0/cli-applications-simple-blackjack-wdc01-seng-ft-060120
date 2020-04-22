bf50f9ffa0f479c944b70836bc843d0add482ca6
David Chung <dbchung3@yahoo.com>
2020-04-22 02:36:54 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -53,12 +53,11 @@ def invalid_command
   puts "Please enter a valid command"
 end
 
-#####################################################
-# get every test to pass before coding runner below #
-#####################################################
 
 def runner
   welcome 
  
 end
+
+binding.pry
     
