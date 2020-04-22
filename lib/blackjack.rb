d1fd6769267da01e208a31ab7b138c8c6118c727
David Chung <dbchung3@yahoo.com>
2020-04-22 01:57:10 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -11,11 +11,11 @@ def display_card_total(total)
 end
 
 def prompt_user
-  
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+  gets.chomp 
 end
 
 def end_game
